a8a5d1b7b85657014ea482fe5d6cb961403773b1
Tom Kay <oridan82@gmail.com>
2015-05-05 17:42:00 +0100

    update packages

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export PATH=~/.composer/vendor/bin:$PATH
-composer global require 'phpunit/phpunit=4.1'
-composer global require 'squizlabs/php_codesniffer=1.*'
-composer global require 'phploc/phploc=*'
-composer global require 'pdepend/pdepend=*'
-composer global require 'phpmd/phpmd=*'
-composer global require 'sebastian/phpcpd=*'
+composer global require 'phpunit/phpunit=*' --prefer-dist
+composer global require 'squizlabs/php_codesniffer=*' --prefer-dist
+composer global require 'phploc/phploc=*' --prefer-dist
+composer global require 'pdepend/pdepend=*' --prefer-dist
+composer global require 'phpmd/phpmd=*' --prefer-dist
+composer global require 'sebastian/phpcpd=*' --prefer-dist
