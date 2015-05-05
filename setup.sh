dd38c1ab93fd668cf260ebdeb164abdcf6ae0112
Tom Kay <oridan82@gmail.com>
2015-05-05 17:46:37 +0100

    more simples

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=~/.composer/vendor/bin:$PATH
 composer selfupdate
-composer global require 'phpunit/phpunit=*' 'squizlabs/php_codesniffer=*' 'phploc/phploc=*' 'pdepend/pdepend=*' 'phpmd/phpmd=*' 'sebastian/phpcpd=*' --prefer-dist
+composer global require phpunit/phpunit squizlabs/php_codesniffer phploc/phploc pdepend/pdepend phpmd/phpmd sebastian/phpcpd --prefer-dist
